0fd27c01297087ed318cedfa76ad19a0550c6596
brinkz <isabel.sbg11@gmail.com>
2022-08-04 17:19:57 -0600

    Se agrego una imagen al canvas

--- villaEvent.js
+++ villaEvent.js
@@ -1,8 +1,17 @@
-var valMax;
-var valMin;
+var vp = document.getElementById("villaPlatzi");
+var papel = vp.getContext("2d");
+var mapa = "tile.webp";
+
+var imagen = new Image();
+imagen.src = mapa;
+imagen.addEventListener("load", dibujar);
+
+function dibujar(){
+    papel.drawImage(imagen, 0, 0);
+}
 
 for(i = 0; i < 10; i++){
-    var z = aleatoreo(10, 20);
+    var z = aleatoreo(30, 140);
     document.write(z + ", ");
 }
 
