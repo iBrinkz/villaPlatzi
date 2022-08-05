a36bc1379509bd82a375797dd2c28fd01b809c14
brinkz <isabel.sbg11@gmail.com>
2022-08-05 13:35:12 -0600

    dibujar cada imagen por separado, no recomendable

--- villaEvent.js
+++ villaEvent.js
@@ -2,17 +2,35 @@ var vp = document.getElementById("villaPlatzi");
 var papel = vp.getContext("2d");
 var mapa = "tile.webp";
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+var fondo = new Image();
+fondo.src = mapa;
+fondo.addEventListener("load", dibujarFondo);
 
-function dibujar(){
-    papel.drawImage(imagen, 0, 0);
+var vaca = new Image();
+vaca.src = "vaca.webp";
+vaca.addEventListener("load", dibujarVaca);
+var cerdo = new Image();
+cerdo.src = "cerdo.webp";
+cerdo.addEventListener("load", dibujarCerdo);
+var pollo = new Image();
+pollo.src = "pollo.webp";
+pollo.addEventListener("load", dibujarPollo);
+
+
+function dibujarFondo(){
+    papel.drawImage(fondo, 0, 0);
 }
 
-for(i = 0; i < 10; i++){
-    var z = aleatoreo(30, 140);
-    document.write(z + ", ");
+function dibujarVaca(){
+    papel.drawImage(vaca, 10, 10);
+}
+
+function dibujarCerdo(){
+    papel.drawImage(cerdo, 10, 300);
+}
+
+function dibujarPollo(){
+    papel.drawImage(pollo, 300, 150);
 }
 
 function aleatoreo(min, max){
