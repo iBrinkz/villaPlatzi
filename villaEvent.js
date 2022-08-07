99c3c1f7da16f9a028bfbd2d7d40f152b5c6c949
brinkz <isabel.sbg11@gmail.com>
2022-08-06 18:02:50 -0600

    Dibujar cantidad y posicion aleatoriamente

--- villaEvent.js
+++ villaEvent.js
@@ -1,6 +1,12 @@
 var vp = document.getElementById("villaPlatzi");
 var papel = vp.getContext("2d");
 
+var x = 0;
+var y = 0;
+var numVacas = aleatoreo(5, 15);
+var numCerdos = aleatoreo(0, 10);
+var numPollos = aleatoreo(3, 12);
+//variables con atributos
 var fondo = {
     url: "tile.webp",
     cargaOK: false
@@ -11,15 +17,31 @@ var vaca = {
     cargaOK: false
 };
 
+var cerdo = {
+    url: "cerdo.webp",
+    cargaOK: false
+};
 
+var pollo = {
+    url: "pollo.webp",
+    cargaOK: false
+};
+//crear una instancia mediante un atributo de una variable
 fondo.imagen = new Image();
-fondo.imagen.src = fondo.url;
+fondo.imagen.src = fondo.url;//asignar la ruta de la imagen
 fondo.imagen.addEventListener("load", cargarFondo);
 
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
 
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
 
 function cargarFondo(){
     fondo.cargaOK = true;
@@ -31,17 +53,44 @@ function cargarVacas(){
     dibujar();
 }
 
+function cargarCerdos(){
+    cerdo.cargaOK = true;
+    dibujar();
+}
+
+function cargarPollos(){
+    pollo.cargaOK = true;
+    dibujar();
+}
+var n = 0;
 function dibujar(){
+    //dibuja 4 veces el fondo ya que hay 3 imagenes mas
     if(fondo.cargaOK){
         papel.drawImage(fondo.imagen, 0, 0);
     }
-    
+        
     if(vaca.cargaOK){
-        papel.drawImage(vaca.imagen, 10, 150);
+        for(i =0; i < numVacas; i++){
+            x = aleatoreo(0, 420);
+            y = aleatoreo(0, 420);
+            papel.drawImage(vaca.imagen, x, y);
+        }
+    }
+    if(cerdo.cargaOK){
+        for(i = 0; i < numCerdos; i++){
+            x = aleatoreo(0, 420);
+            y = aleatoreo(0, 420);
+            papel.drawImage(cerdo.imagen, x, y);
+        }
+    }
+    if(pollo.cargaOK){
+        for(i = 0; i < numPollos; i++){
+            x = aleatoreo(0, 420);
+            y = aleatoreo(0, 420);
+            papel.drawImage(pollo.imagen, x, y);
+        }
     }
 }
-
-
 
 function aleatoreo(min, max){
     resultado = Math.floor(Math.random() * (max - min + 1)) + min;
